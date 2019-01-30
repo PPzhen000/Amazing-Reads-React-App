a0b440b951085c10d4e6dbe0cc050e980d911f0e
Ariel <pzhen001@g.ucla.edu>
2019-01-30 00:18:06 -0800

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create(title: "Where the Crawdads Sing",
+            author: "DELIA OWENS",
+            description: "Painfully beautiful.",
+            image_url: "https://images-na.ssl-images-amazon.com/images/I/51j5p18mJNL._SY346_.jpg")
+
+Book.create(title: "Harry Potter and the Sorcerer's Stone",
+            author: "J.K. ROWLING",
+            description: "Harry Potter has never even heard of Hogwarts when the letters start dropping on the doormat at number four, Privet Drive. Addressed in green ink on yellowish parchment with a purple seal, they are swiftly confiscated by his grisly aunt and uncle. Then, on Harry's eleventh birthday, a great beetle-eyed giant of a man called Rubeus Hagrid bursts in with some astonishing news: Harry Potter is a wizard, and he has a place at Hogwarts School of Witchcraft and Wizardry. An incredible adventure is about to begin!",
+            image_url: "https://m.media-amazon.com/images/I/91G2aMQuu9L._SX140_.jpg")
+
+Book.create(title: "Harry Potter and the Order of the Phoenix",
+            author: "J.K. ROWLING",
+            description: "90 WEEKS ON THE LIST",
+            image_url: "https://m.media-amazon.com/images/I/712nNwaEpzL._SX140_.jpg")
+
+Book.create(title: "Becoming",
+            author: "MICHELLE OBAMA",
+            description: "11 WEEKS ON THE LIST",
+            image_url: "https://m.media-amazon.com/images/I/81h2gWPTYJL._SX140_.jpg")
+
+Book.create(title: "Can't Hurt Me",
+            author: "DAVID GOGGINS",
+            description: "8 WEEKS ON THE LIST",
+            image_url: "https://m.media-amazon.com/images/I/81eO3s9VJ4L._SX140_.jpg")
+
+Book.create(title: "Educated",
+            author: " TARA WESTOVER",
+            description: "48 WEEKS ON THE LIST",
+            image_url: "https://m.media-amazon.com/images/I/81PoJHSAznL._SX140_.jpg")
+
+Book.create(title: "Girl, Wash Your Face",
+            author: "RACHEL HOLLIS",
+            description: "42 WEEKS ON THE LIST",
+            image_url: "https://m.media-amazon.com/images/I/91FStYY4sML._SX140_.jpg")
+
+Book.create(title: "12 Rules for Life",
+            author: "JORDAN B. PETERSON",
+            description: "53 WEEKS ON THE LIST",
+            image_url: "https://m.media-amazon.com/images/I/71KBsWtC6RL._SX140_.jpg")
