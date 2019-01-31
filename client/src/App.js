1487779c4afb5b45db35e96f29969af1160627ca
Ariel <pzhen001@g.ucla.edu>
2019-01-31 00:22:11 -0800

    create constructor method for BooksContainer

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar';
+import BooksContainer from './containers/BooksContainer';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <NavBar />
+        <BooksContainer />
       </div>
     );
   }
--- client/src/containers/BooksContainer.js
+++ client/src/containers/BooksContainer.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 import { fetchBooks } from '../actions/bookActions';
 
 class BooksContainer extends Component {
-    render() {
-        return (
-            <div className="books-container">
-                Books
-            </div>
-        )
+  constructor(props) {
+    super(props)
+    this.state = {
+      books: []
     }
+  }
+
+  componentDidMount() {
+
+  }
+  
+  render() {
+    return (
+      <div className="books-container">
+        Books
+      </div>
+    )
+  }
 }
 
 export default BooksContainer;
