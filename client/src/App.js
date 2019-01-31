ca0c67bc94469b92af72efa0fd14e1a34b393366
Ariel <pzhen001@g.ucla.edu>
2019-01-31 14:03:11 -0800

    fix NewBookForm typo

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar';
 import BooksContainer from './containers/BooksContainer';
+import NewBookForm from './containers/NewBookForm';
+
 import './App.css';
 
 class App extends Component {
