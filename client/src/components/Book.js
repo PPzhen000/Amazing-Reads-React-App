c49ccab89a88c5d450353b8bef83dee0312d441d
Ariel <pzhen001@g.ucla.edu>
2019-01-31 00:24:57 -0800

    create Book.js

---
+++ client/src/components/Book.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Book extends Component {
+  render() {
+    return (
+      <div className="book">
+        Each book 
+      </div>
+    )
+  }
+}
