bc40f6b513c4345a538f3b08ed30c640311d4cdf
Ariel <pzhen001@g.ucla.edu>
2019-02-01 01:03:04 -0800

    implement routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import BooksContainer from './containers/BooksContainer';
 import NewBookForm from './containers/NewBookForm';
@@ -12,8 +13,12 @@ class App extends Component {
       <Provider store={store}>
         <div className="App">
           <NavBar />
-          <NewBookForm />
-          <BooksContainer />
+          <Router>
+            <React.Fragment>
+              <Route path="/new" component={NewBookForm} />
+              <Route path="/" component={BooksContainer} />
+            </React.Fragment>
+          </Router>
         </div>
       </Provider>
     );
