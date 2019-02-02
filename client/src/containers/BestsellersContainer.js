fad37c4f5a7ce348a8e3b266c95001cde0ecec35
Ariel <pzhen001@g.ucla.edu>
2019-02-02 13:41:33 -0800

    move NewBookForm to components folder and update BestsellersContainer to include nonfiction option

--- client/src/containers/BestsellersContainer.js
+++ client/src/containers/BestsellersContainer.js
@@ -23,6 +23,8 @@ class BestSellersContainer extends Component {
     return (
       <div className="books-container">
         <h2>New York Times Best Sellers List</h2>
+        <h3>Combined Print & E-Book best sellers</h3>
+        <h3>Fiction | Nonfiction</h3>
         {bestsellers}
       </div>
     )
