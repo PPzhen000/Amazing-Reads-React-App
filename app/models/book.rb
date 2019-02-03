e5a99a600febc2e47a8273839efb20f550b6e958
Ariel <pzhen001@g.ucla.edu>
2019-02-02 23:54:17 -0800

    add validations for book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
   has_many :lists, dependent: :destroy
   has_many :users, through: :lists
+
+  validates :title, :author, :description, :image_url, presence: true
 end
