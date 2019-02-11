166dd9f5092aeb13f6fe2f5727f9134355073735
Ariel <pzhen001@g.ucla.edu>
2019-02-11 00:21:25 -0800

    create auth.rb for user authentication

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,13 +1,6 @@
 class Api::UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
-  # GET /users
-  def index
-    @users = User.all
-
-    render json: @users
-  end
-
   # GET /users/1
   def show
     render json: @user
@@ -46,6 +39,6 @@ class Api::UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:login, :name)
+      params.require(:user).permit(:username, :password)
     end
 end
