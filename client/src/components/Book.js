dbb7b87572dcfdbcd290ed04dc2ed2d8661c559a
Ariel <pzhen001@g.ucla.edu>
2019-02-19 12:30:17 -0800

    refractor Book.js to use functional component

--- client/src/components/Book.js
+++ client/src/components/Book.js
@@ -4,33 +4,58 @@ import LikeButton from '../containers/LikeButton';
 import './Book.css';
 import { Link } from 'react-router-dom'
 
-class Book extends Component {
-  static propTypes = {
-    id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    author: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    image_url: PropTypes.string.isRequired,
-    onDelete: PropTypes.func.isRequired,
-  }
-  render() {
-    const { id, title, author, description, image_url, onDelete } = this.props;
-    return (
-      <div className="book-card">
-        <button className="delete-button" onClick={() => onDelete(id)}>X</button>
-        <Link to={`/books/${id}`}>
-          <div className="book-img">
-            <img src={image_url} alt={title} />
-          </div>
-        </Link>
-        <div className="book-info">
-          {<h2 className="book-title">{title}</h2>}
-          <h5>{author}</h5>
-        </div>
-        <LikeButton />
+const Book = ( { id, title, author, description, image_url, onDelete } ) => (
+  <div className="book-card">
+    <button className="delete-button" onClick={() => onDelete(id)}>X</button>
+    <Link to={`/books/${id}`}>
+      <div className="book-img">
+        <img src={image_url} alt={title} />
       </div>
-    )
-  }
-}
+    </Link>
+    <div className="book-info">
+      {<h2 className="book-title">{title}</h2>}
+      <h5>{author}</h5>
+    </div>
+    <LikeButton />
+  </div>
+)
+
+Book.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  image_url: PropTypes.string.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
+
+// class Book extends Component {
+//   static propTypes = {
+//     id: PropTypes.number.isRequired,
+//     title: PropTypes.string.isRequired,
+//     author: PropTypes.string.isRequired,
+//     description: PropTypes.string.isRequired,
+//     image_url: PropTypes.string.isRequired,
+//     onDelete: PropTypes.func.isRequired,
+//   }
+//   render() {
+//     const { id, title, author, description, image_url, onDelete } = this.props;
+//     return (
+//       <div className="book-card">
+//         <button className="delete-button" onClick={() => onDelete(id)}>X</button>
+//         <Link to={`/books/${id}`}>
+//           <div className="book-img">
+//             <img src={image_url} alt={title} />
+//           </div>
+//         </Link>
+//         <div className="book-info">
+//           {<h2 className="book-title">{title}</h2>}
+//           <h5>{author}</h5>
+//         </div>
+//         <LikeButton />
+//       </div>
+//     )
+//   }
+// }
 
 export default Book;
