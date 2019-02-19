d75ee62d6f449c811cb2927ce7f425679911b12e
Ariel <pzhen001@g.ucla.edu>
2019-02-19 12:21:45 -0800

    update Navbar.js to use functional component

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,22 +1,18 @@
 import React, {Component} from 'react';
 import './NavBar.css';
-
 import { Link } from 'react-router-dom'
 
-class Navbar extends Component {
-  render() {
-    return (
-      <header>
-        <h2><a>Amazing Reads</a></h2>
-        <nav>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/'>Your Library</Link></li>
-          <li><Link to='/bestsellers'>Bestsellers</Link></li>
-          <li><Link to='/new'>New Book</Link></li>
-        </nav>
-      </header>
-    );
-  }
+const Navbar = () => {
+  return (
+    <header>
+      <h2><a>Amazing Reads</a></h2>
+      <nav>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/'>Your Library</Link></li>
+        <li><Link to='/bestsellers'>Bestsellers</Link></li>
+        <li><Link to='/new'>New Book</Link></li>
+      </nav>
+    </header>
+  )
 }
-
 export default Navbar;
